ca50f65a2dda3cc776b085c7b5ba0cf725205726
정석원 <boss315@startergames.com>
2026-03-06 14:29:35 +0900

    VContainer 설치

---
+++ Assets/Script/DataBase/Interface/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Script.DataBase.Interface {
+    /// <summary>
+    /// 유저 데이터를 저장할 인터페이스
+    /// 현재는 따로 서버 구현 예정이 없기 때문에 Json으로 저장 목표
+    /// </summary>
+    public interface IRepository {
+        void Initialize();
+    }
+}
