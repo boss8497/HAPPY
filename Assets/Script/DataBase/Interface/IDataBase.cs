27b8e276d567c898d5ad0b7377c38e1a6cffdb86
정석원 <boss315@startergames.com>
2026-03-07 17:18:45 +0900

    MessagePack 설치 및 기본 데이터에 저장할 테스트 모델 설정

--- Assets/Script/DataBase/Interface/IDataBase.cs
+++ Assets/Script/DataBase/Interface/IDataBase.cs
@@ -6,6 +6,7 @@ namespace Script.DataBase.Interface {
     /// <summary>
     /// 유저 데이터를 저장할 인터페이스
     /// 현재는 따로 서버 구현 예정이 없기 때문에 json 으로 저장 목표
+    /// 테스트 시 json으로 저장하고 나중에는 MessagePack으로 변경할 예정
     /// </summary>
     public interface IDataBase{
         bool IsInitialized { get; }
--- Assets/Script/DataBase/Interface/IRepository.cs
+++ Assets/Script/DataBase/Interface/IRepository.cs
@@ -1,9 +1,4 @@
-﻿
-/// <summary>
-/// 유저 데이터를 저장할 인터페이스
-/// 현재는 따로 서버 구현 예정이 없기 때문에 Json으로 저장 목표
-/// </summary>
-namespace Script.DataBase.Interface {
+﻿namespace Script.DataBase.Interface {
     public interface IRepository {
         void Initialize();
     }
