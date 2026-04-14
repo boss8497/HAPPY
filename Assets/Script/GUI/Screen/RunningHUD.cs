198a0748659d7a29ac44f6a77b234fb526cc4a00
정석원 <boss315@startergames.com>
2026-04-14 12:52:29 +0900

    플레이어 Hp Ui 추가

--- Assets/Script/GUI/Screen/RunningHUD.cs
+++ Assets/Script/GUI/Screen/RunningHUD.cs
@@ -1,29 +1,118 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using Cysharp.Threading.Tasks;
+using R3;
 using Script.GameInfo.Attribute;
 using Script.GamePlay.Stage;
 using Script.Utility.Runtime;
+using UnityEngine;
 using UnityEngine.UI;
 using VContainer;
 
 namespace Script.GUI.Screen {
     public class RunningHUD : Screen {
+        [System.Serializable]
+        public class PlayerHpObject {
+            public GameObject root;
+            public GameObject hp;
+        }
+
+
+        /// <summary>
+        /// Inject
+        /// </summary>
+        private IStageManager _stageManager;
+
+        [Inject]
+        public void InjectSelf(
+            IStageManager stageManager
+        ) {
+            _stageManager = stageManager;
+        }
+        
+        
+
+
+        /// <summary>
+        /// Inspector
+        /// </summary>
         [ScreenKey]
         public string screenKey;
 
         public Button optionBtn;
 
+        public PlayerHpObject[] playerHps = Array.Empty<PlayerHpObject>();
+
+
+        /// <summary>
+        /// private
+        /// </summary>
+        private DisposableBag _disposableBag;
+
+        private CancellationTokenSource _addPlayerCts;
+
         protected override void AwakeInternal() {
             base.AwakeInternal();
-            
             optionBtn.ClickAddListener(OpenOption);
+            SetHp(0, 0);
         }
 
         public override UniTask OpenInternal() {
+            _disposableBag.Dispose();
+            _disposableBag = new();
+
+            // 플레이어 추가
+            StopAddPlayer();
+            _addPlayerCts = new();
+            AddPlayer(_addPlayerCts.Token).Forget();
+
             return UniTask.CompletedTask;
         }
+
         public override UniTask CloseInternal() {
+            StopAddPlayer();
+            _disposableBag.Dispose();
             return UniTask.CompletedTask;
+        }
+
+        private async UniTask AddPlayer(CancellationToken ct) {
+            if (_stageManager == null) {
+                Debug.LogError($"StageManager가 주입되지 않았습니다.");
+                return;
+            }
+            
+            await UniTask.WaitUntil(() => _stageManager.Initialized?.CurrentValue ?? false, cancellationToken: ct);
+            await UniTask.WaitUntil(() => (_stageManager.Players?.Count ?? 0) > 0, cancellationToken: ct);
+
+            // Running에서는 플레이어 캐릭터가 1명
+            var player = _stageManager.Players.FirstOrDefault();
+            
+            // Null 체크 안한다. 여기서 Player가 Null이면 이미 문제이기 때문.
+            SetHp((int)player.MaxHealth.CurrentValue, (int)player.Health.CurrentValue);
+            
+            player.MaxHealth.CombineLatest(player.Health, (maxHp, hp) => (maxHp, hp))
+                        .Subscribe(tuple => {
+                            SetHp((int)tuple.maxHp, (int)tuple.hp);
+                        })
+                        .AddTo(ref _disposableBag);
+
+        }
+
+        private void StopAddPlayer() {
+            if (_addPlayerCts is { IsCancellationRequested: false }) {
+                _addPlayerCts.Cancel();
+                _addPlayerCts.Dispose();
+                _addPlayerCts = null;
+            }
+        }
+
+        private void SetHp(int maxHp, int hp) {
+            for (int i = 0; i < playerHps.Length; i++) {
+                var hpObj = playerHps[i];
+                hpObj.root.SetActiveSafe(i < maxHp);
+                hpObj.hp.SetActiveSafe(i < hp);
+            }
         }
 
         private void OpenOption() {
