47e561bd3a143b9357a0c48e38d10740ced4ceed
정석원 <boss315@startergames.com>
2026-04-17 18:13:05 +0900

    데이터 누락된거 추가

--- Assets/Script/GameInfo/Character/CharacterInfo.cs
+++ Assets/Script/GameInfo/Character/CharacterInfo.cs
@@ -1,6 +1,7 @@
 using System;
 using Script.GameInfo.Base;
 using Script.GameInfo.Attribute;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Script.GameInfo.Character {
@@ -17,6 +18,8 @@ namespace Script.GameInfo.Character {
         [Status]
         public int[] statusUids = Array.Empty<int>();
 
+        [ShowIf("@type == CharacterType.Buff"), Buff]
+        public int[] buffUids = Array.Empty<int>();
 
         [AssetPath(typeof(GameObject))]
         public string prefab;
