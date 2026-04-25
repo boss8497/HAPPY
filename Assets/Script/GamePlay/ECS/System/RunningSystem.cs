df489403823391cb1ba78cb27674b1fed28bcdcf
정석원 <boss315@startergames.com>
2026-04-25 21:47:47 +0900

    Running Job 추가

--- Assets/Script/GamePlay/ECS/System/RunningSystem.cs
+++ Assets/Script/GamePlay/ECS/System/RunningSystem.cs
@@ -9,27 +9,39 @@ namespace Script.GamePlay.ECS.System {
     [BurstCompile]
     [UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
     public partial struct RunningSystem : ISystem {
+        private EntityQuery _query;
+
         [BurstCompile]
         public void OnCreate(ref SystemState state) {
-            state.RequireForUpdate<RunningData>();
+            _query = SystemAPI.QueryBuilder()
+                              .WithAllRW<LocalTransform>()
+                              .WithAll<RunningData>()
+                              .WithDisabled<UnitDieTag>()
+                              .Build();
+
+            state.RequireForUpdate(_query);
         }
 
         [BurstCompile]
         public void OnUpdate(ref SystemState state) {
-            // FixedStepSimulationSystemGroup은 업데이트 동안 Time.DeltaTime을 고정 스텝 값으로 override한다.
-            // 그래서 fixed-step 안에서는 DeltaTime이 “그 프레임에 실제 사용된 fixed step”이 된다.
             var fixedTime = SystemAPI.Time.DeltaTime;
 
-            foreach (var (transform, running) in
-                     SystemAPI.Query<RefRW<LocalTransform>, RefRO<RunningData>>()
-                              .WithDisabled<UnitDieTag>()) {
-                float3 dir = running.ValueRO.Direction;
+            state.Dependency = new RunningJob {
+                DeltaTime = fixedTime
+            }.ScheduleParallel(_query, state.Dependency);
+        }
 
-                // 혹시 0벡터가 들어오면 이동 안 함
+        [BurstCompile]
+        private partial struct RunningJob : IJobEntity {
+            public float DeltaTime;
+
+            private void Execute(ref LocalTransform transform, in RunningData running) {
+                var dir = running.Direction;
+
                 if (math.lengthsq(dir) <= 0.000001f)
-                    continue;
+                    return;
 
-                transform.ValueRW.Position += dir * running.ValueRO.Speed * fixedTime;
+                transform.Position += dir * running.Speed * DeltaTime;
             }
         }
     }
