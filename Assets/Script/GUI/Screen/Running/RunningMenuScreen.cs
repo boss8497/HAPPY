86460a0eb66a624e0c74f7487d3d6524957a5188
정석원 <boss315@startergames.com>
2026-05-03 14:00:29 +0900

    인게임에서 로비로 돌아갈 수 있도록 설정

--- Assets/Script/GUI/Screen/Running/RunningMenuScreen.cs
+++ Assets/Script/GUI/Screen/Running/RunningMenuScreen.cs
@@ -1,5 +1,7 @@
 ﻿using Cysharp.Threading.Tasks;
+using Script.GamePlay.Scene;
 using Script.GamePlay.Stage;
+using Script.GameSetting.Interface;
 using Script.Utility.Runtime;
 using UnityEngine.UI;
 using VContainer;
@@ -7,20 +9,45 @@ using VContainer;
 namespace Script.GUI.Screen {
     public class RunningMenuScreen : Screen {
         
-        public Button restartBtn;
+        // Private
+        private bool _enterLobby = false;
         
+        // Inspector
+        public Button restartBtn;
+        public Button lobbyBtn;
+        
+        // Inject
         private IStageManager _stageManager;
+        private ISceneLoader  _sceneLoader;
+        private IGameSetting  _gameSetting;
 
         [Inject]
         public void InjectSelf(
-            IStageManager stageManager
+            IStageManager stageManager,
+            IGameSetting gameSetting,
+            ISceneLoader sceneLoader
         ) {
             _stageManager = stageManager;
+            _gameSetting  = gameSetting;
+            _sceneLoader  = sceneLoader;
         }
 
         protected override void AwakeInternal() {
             base.AwakeInternal();
             restartBtn.ClickAddListener(Restart, false);
+            lobbyBtn.ClickAddListener(EnterLobby);
+        }
+        
+        private void EnterLobby() {
+            if (_enterLobby) return;
+            _enterLobby = true;
+            EnterLobbyAsync().Forget();
+        }
+
+        private async UniTask EnterLobbyAsync() {
+            await _sceneLoader.LoadScene(_gameSetting.LobbyScenePath);
+
+            _enterLobby = false;
         }
 
         private void Restart() {
