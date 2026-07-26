f2c7f18de1177adef49d53a9d3a792c46d3c6ea8
정석원 <boss315@startergames.com>
2026-07-27 00:38:00 +0900

    Option Screen 만들기 위한 준비

---
+++ Assets/Script/GUI/Screen/PopUp/OptionScreen.cs
@@ -0,0 +1,14 @@
+﻿using Cysharp.Threading.Tasks;
+using Script.GUI.ScreenData.Interface;
+
+namespace Script.GUI.Screen {
+    public class OptionScreen : Screen {
+        public override UniTask OpenInternal(IScreenOption screenOption) {
+            throw new System.NotImplementedException();
+        }
+
+        public override UniTask CloseInternal() {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Assets/Script/GamePlay/Audio/Data/Model/AudioSettingModel.cs
+++ Assets/Script/GamePlay/Audio/Data/Model/AudioSettingModel.cs
@@ -1,3 +1,4 @@
+using System;
 using MessagePack;
 
 namespace Script.GamePlay.Audio.Model {
@@ -6,7 +7,7 @@ namespace Script.GamePlay.Audio.Model {
     /// 추후 MessagePack 전환을 대비해 Key 어트리뷰트를 붙여둔다.
     /// </summary>
     [MessagePackObject]
-    public class AudioSettingModel {
+    public class AudioSettingModel : ICloneable, IEquatable<AudioSettingModel> {
         [Key(0)] public float masterVolume = 1f;
         [Key(1)] public float bgmVolume    = 0.7f;
         [Key(2)] public float effectVolume = 1f;
@@ -18,5 +19,42 @@ namespace Script.GamePlay.Audio.Model {
         [Key(7)] public bool voiceMute;
 
         public static AudioSettingModel CreateDefault() => new();
+
+        public object Clone() {
+            return new AudioSettingModel {
+                masterVolume = masterVolume,
+                bgmVolume    = bgmVolume,
+                effectVolume = effectVolume,
+                voiceVolume  = voiceVolume,
+                masterMute   = masterMute,
+                bgmMute      = bgmMute,
+                effectMute   = effectMute,
+                voiceMute    = voiceMute,
+            };
+        }
+
+        public bool Equals(AudioSettingModel other) {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return masterVolume.Equals(other.masterVolume)
+                && bgmVolume.Equals(other.bgmVolume)
+                && effectVolume.Equals(other.effectVolume)
+                && voiceVolume.Equals(other.voiceVolume)
+                && masterMute == other.masterMute
+                && bgmMute == other.bgmMute
+                && effectMute == other.effectMute
+                && voiceMute == other.voiceMute;
+        }
+
+        public override bool Equals(object obj) {
+            return obj is AudioSettingModel other && Equals(other);
+        }
+
+        public override int GetHashCode() {
+            return HashCode.Combine(
+                masterVolume, bgmVolume, effectVolume, voiceVolume,
+                masterMute, bgmMute, effectMute, voiceMute);
+        }
     }
 }
