2e292b67ef867309ba2d2585ae1ba0965737bd44
정석원 <boss315@startergames.com>
2026-08-03 12:24:41 +0900

    오디오 옵션 마무리

--- Assets/Script/GUI/ViewModel/OptionModel/AudioOptionViewModel.cs
+++ Assets/Script/GUI/ViewModel/OptionModel/AudioOptionViewModel.cs
@@ -35,6 +35,15 @@ namespace Script.GUI.ViewModel {
         public ReactiveProperty<float> MasterVolume     { get; set; }
         public ReactiveProperty<bool>  MasterVolumeMute { get; set; }
 
+        public ReactiveProperty<float> BgmVolume     { get; set; }
+        public ReactiveProperty<bool>  BgmVolumeMute { get; set; }
+
+        public ReactiveProperty<float> EffectVolume     { get; set; }
+        public ReactiveProperty<bool>  EffectVolumeMute { get; set; }
+
+        public ReactiveProperty<float> VoiceVolume     { get; set; }
+        public ReactiveProperty<bool>  VoiceVolumeMute { get; set; }
+
         private DisposableBag _disposableBag;
 
         [Inject]
@@ -54,11 +63,20 @@ namespace Script.GUI.ViewModel {
         private void InitializeReactiveProperty() {
             _disposableBag.Dispose();
             _disposableBag = new();
-            
-            UpdateChanged  = new();
+
+            UpdateChanged = new();
 
             MasterVolume     = new(-1);
             MasterVolumeMute = new();
+
+            BgmVolume     = new(-1);
+            BgmVolumeMute = new();
+
+            EffectVolume     = new(-1);
+            EffectVolumeMute = new();
+
+            VoiceVolume     = new(-1);
+            VoiceVolumeMute = new();
 
             BackUpAudioSetting = AudioManager.Select(i => i?.AudioSettingModel == null ? Observable.Empty<AudioSettingModel>() : Observable.Return(i.AudioSettingModel.Clone() as AudioSettingModel))
                                              .Switch()
@@ -79,6 +97,14 @@ namespace Script.GUI.ViewModel {
                                masterVolume.value = setting.masterVolume;
                                masterVolumeMute.SetIsOn(setting.masterMute, true);
 
+                               bgmVolume.value = setting.bgmVolume;
+                               bgmVolumeMute.SetIsOn(setting.bgmMute, true);
+
+                               effectVolume.value = setting.effectVolume;
+                               effectVolumeMute.SetIsOn(setting.effectMute, true);
+
+                               voiceVolume.value = setting.voiceVolume;
+                               voiceVolumeMute.SetIsOn(setting.voiceMute, true);
 
                                return Observable.Return(setting);
                            })
@@ -117,6 +143,75 @@ namespace Script.GUI.ViewModel {
                             .AddTo(ref _disposableBag);
 
 
+            BgmVolume.CombineLatest(AudioSetting, IsInitialized, (volume, setting, init) => (volume, setting, init))
+                     .Subscribe((volume) => {
+                         if (volume.setting == null || volume.init == false || volume.volume < 0) return;
+
+                         if (Mathf.Abs(volume.setting.bgmVolume - volume.volume) > float.Epsilon) {
+                             AudioManager.Value.SetVolume(AudioGroup.BGM, volume.volume, false);
+                             UpdateChanged.ForceNotify();
+                         }
+                     })
+                     .AddTo(ref _disposableBag);
+
+            BgmVolumeMute.CombineLatest(AudioSetting, (mute, setting) => (mute, setting))
+                         .Subscribe((volume) => {
+                             if (volume.setting == null) return;
+
+                             if (volume.setting.bgmMute != volume.mute) {
+                                 AudioManager.Value.SetMute(AudioGroup.BGM, volume.mute, false);
+                                 UpdateChanged.ForceNotify();
+                             }
+                         })
+                         .AddTo(ref _disposableBag);
+
+
+            EffectVolume.CombineLatest(AudioSetting, IsInitialized, (volume, setting, init) => (volume, setting, init))
+                        .Subscribe((volume) => {
+                            if (volume.setting == null || volume.init == false || volume.volume < 0) return;
+
+                            if (Mathf.Abs(volume.setting.effectVolume - volume.volume) > float.Epsilon) {
+                                AudioManager.Value.SetVolume(AudioGroup.Effect, volume.volume, false);
+                                UpdateChanged.ForceNotify();
+                            }
+                        })
+                        .AddTo(ref _disposableBag);
+
+            EffectVolumeMute.CombineLatest(AudioSetting, (mute, setting) => (mute, setting))
+                            .Subscribe((volume) => {
+                                if (volume.setting == null) return;
+
+                                if (volume.setting.effectMute != volume.mute) {
+                                    AudioManager.Value.SetMute(AudioGroup.Effect, volume.mute, false);
+                                    UpdateChanged.ForceNotify();
+                                }
+                            })
+                            .AddTo(ref _disposableBag);
+
+
+            VoiceVolume.CombineLatest(AudioSetting, IsInitialized, (volume, setting, init) => (volume, setting, init))
+                       .Subscribe((volume) => {
+                           if (volume.setting == null || volume.init == false || volume.volume < 0) return;
+
+                           if (Mathf.Abs(volume.setting.voiceVolume - volume.volume) > float.Epsilon) {
+                               AudioManager.Value.SetVolume(AudioGroup.Voice, volume.volume, false);
+                               UpdateChanged.ForceNotify();
+                           }
+                       })
+                       .AddTo(ref _disposableBag);
+
+            VoiceVolumeMute.CombineLatest(AudioSetting, (mute, setting) => (mute, setting))
+                           .Subscribe((volume) => {
+                               if (volume.setting == null) return;
+
+                               if (volume.setting.voiceMute != volume.mute) {
+                                   AudioManager.Value.SetMute(AudioGroup.Voice, volume.mute, false);
+                                   UpdateChanged.ForceNotify();
+                               }
+                           })
+                           .AddTo(ref _disposableBag);
+
+
             AudioManager.Subscribe(audioManager => {
                             if (audioManager != null) {
                                 AddState(ViewModelState.Initialized);
@@ -130,6 +225,20 @@ namespace Script.GUI.ViewModel {
         private void EventSetting() {
             masterVolume.onValueChanged.RemoveAllListeners();
             masterVolume.onValueChanged.AddListener((value) => { MasterVolume.OnNext(value); });
+
+            bgmVolume.onValueChanged.RemoveAllListeners();
+            bgmVolume.onValueChanged.AddListener((value) => { BgmVolume.OnNext(value); });
+
+            effectVolume.onValueChanged.RemoveAllListeners();
+            effectVolume.onValueChanged.AddListener((value) => { EffectVolume.OnNext(value); });
+
+            voiceVolume.onValueChanged.RemoveAllListeners();
+            voiceVolume.onValueChanged.AddListener((value) => { VoiceVolume.OnNext(value); });
+
+            masterVolumeMute.AddValueChangedListener((value) => { MasterVolumeMute.OnNext(value); });
+            bgmVolumeMute.AddValueChangedListener((value) => { BgmVolumeMute.OnNext(value); });
+            effectVolumeMute.AddValueChangedListener((value) => { EffectVolumeMute.OnNext(value); });
+            voiceVolumeMute.AddValueChangedListener((value) => { VoiceVolumeMute.OnNext(value); });
         }
 
         public override void DisableInternal() {
@@ -140,6 +249,9 @@ namespace Script.GUI.ViewModel {
             _disposableBag.Dispose();
 
             masterVolume.onValueChanged.RemoveAllListeners();
+            bgmVolume.onValueChanged.RemoveAllListeners();
+            effectVolume.onValueChanged.RemoveAllListeners();
+            voiceVolume.onValueChanged.RemoveAllListeners();
 
             BackUpAudioSetting = null;
             AudioSetting       = null;
@@ -147,6 +259,15 @@ namespace Script.GUI.ViewModel {
 
             MasterVolume     = null;
             MasterVolumeMute = null;
+
+            BgmVolume     = null;
+            BgmVolumeMute = null;
+
+            EffectVolume     = null;
+            EffectVolumeMute = null;
+
+            VoiceVolume     = null;
+            VoiceVolumeMute = null;
         }
 
         public override void DisposeInternal() {
